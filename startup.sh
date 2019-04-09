eb0e315626236cb6aec705954ac90e29858dcfc2
Brent Langston <blangs@amazon.com>
2019-04-09 19:35:41 +0800

    updating AZ detection

--- startup.sh
+++ startup.sh
@@ -1,28 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 
 IP=$(ip route show |grep -o src.* |cut -f2 -d" ")
+SUBNET=$(echo ${IP} | cut -f1 -d.)
 NETWORK=$(echo ${IP} | cut -f3 -d.)
 
-case "${NETWORK}" in
-  100)
-    zone=a
-    color=Crimson
-    ;;
-  101)
-    zone=b
-    color=CornflowerBlue
-    ;;
-  102)
-    zone=c
-    color=LightGreen
-    ;;
-  *)
-    zone=unknown
-    color=Yellow
-    ;;
+case "${SUBNET}" in
+    10)
+        orchestrator=ecs
+        ;;
+    192)
+        orchestrator=kubernetes
+        ;;
+    *)
+        orchestrator=unknown
+        ;;
 esac
+
+if [[ "${orchestrator}" == 'ecs' ]]; then
+    case "${NETWORK}" in
+      100)
+        zone=a
+        color=Crimson
+        ;;
+      101)
+        zone=b
+        color=CornflowerBlue
+        ;;
+      102)
+        zone=c
+        color=LightGreen
+        ;;
+      *)
+        zone=unknown
+        color=Yellow
+        ;;
+    esac
+fi
+
+if [[ "${orchestrator}" == 'kubernetes' ]]; then
+    if ((0<=${NETWORK} && ${NETWORK}<32))
+        then
+            zone=a
+    elif ((32<=${NETWORK} && ${NETWORK}<64))
+        then
+            zone=b
+    elif ((64<=${NETWORK} && ${NETWORK}<96))
+        then
+            zone=c
+    elif ((96<=${NETWORK} && ${NETWORK}<128))
+        then
+            zone=a
+    elif ((128<=${NETWORK} && ${NETWORK}<160))
+        then
+            zone=b
+    elif ((160<=${NETWORK}))
+        then
+            zone=c
+    else
+        zone=unknown
+    fi
+fi 
+
+if [[ ${orchestrator} == 'unknown' ]]; then
+  zone=$(curl -m2 -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.availabilityZone' | grep -o .$)
+fi 
 
 # kubernetes sets routes differently -- so we will discover our IP differently
 if [[ ${IP} == "" ]]; then
