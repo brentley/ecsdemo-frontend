4bfd06b386861f542e81cc5ecbd156155f82435a
Brent Langston <brentley@oufan.com>
2019-08-29 22:39:08 +0000

    updating gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
   config.serve_static_files = true
   config.static_cache_control = "public, max-age=172800"
 
