de94abaa0179fa8d7f589810fb12313a479ed6b1
Adam Keller <keladam@amazon.com>
2020-02-25 04:16:52 +0000

    Moved to construct for base platform

--- cdk/app.py
+++ cdk/app.py
@@ -12,9 +12,9 @@ from aws_cdk import (
 from os import getenv
 
 
-class FrontendService(core.Stack):
+class BasePlatform(core.Construct):
     
-    def __init__(self, scope: core.Stack, id: str, **kwargs):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
         # The base platform stack is where the VPC was created, so all we need is the name to do a lookup and import it into this stack for use
@@ -25,52 +25,51 @@ class FrontendService(core.Stack):
         
         self.sd_namespace = aws_servicediscovery.PrivateDnsNamespace.from_private_dns_namespace_attributes(
             self, "SDNamespace",
-            namespace_name='service'
+            namespace_name=core.Fn.import_value('NSNAME'),
+            namespace_arn=core.Fn.import_value('NSARN'),
+            namespace_id=core.Fn.import_value('NSID')
+        )
+        
+        self.ecs_cluster = aws_ecs.Cluster.from_cluster_attributes(
+            self, "ECSCluster",
+            cluster_name=core.Fn.import_value('ECSClusterName'),
+            security_groups=[],
+            vpc=self.vpc,
+            default_cloud_map_namespace=self.sd_namespace
+        )
+        
+
+class FrontendService(core.Stack):
+    
+    def __init__(self, scope: core.Stack, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        self.base_platform = BasePlatform(self, self.stack_name)
+
+        self.fargate_task_image = aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+            image=aws_ecs.ContainerImage.from_registry("brentley/ecsdemo-frontend"),
+            container_port=3000,
+            environment={
+                "CRYSTAL_URL": "http://ecsdemo-crystal.service:3000/crystal",
+                "NODEJS_URL": "http://ecsdemo-nodejs.service:3000"
+            },
         )
 
-        #self.fargate_load_balanced_service = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
-        #    self, "FrontendFargateLBService",
-        #    vpc=self.vpc,
-        #    image=aws_ecs.ContainerImage.from_registry("brentley/ecsdemo-frontend"),
-        #    container_port=3000,
-        #    cpu=256,
-        #    memory_limit_mib=512,
-        #    enable_logging=True,
-        #    desired_count=1,
-        #    public_load_balancer=True,
-        #    environment={
-        #        "CRYSTAL_URL": "http://ecsdemo-crystal.service:3000/crystal",
-        #        "NODEJS_URL": "http://ecsdemo-nodejs.service:3000"
-        #    },
-        #    cloud_map_options=
-        #)
-#
-#
-        ## There has to be a better way, but for now this is what we got.
-        ## Allow inbound 3000 from Frontend Service to Backend
-        #self.sec_grp_ingress_backend_to_frontend_3000 = aws_ec2.CfnSecurityGroupIngress(
-        #    self, "InboundBackendSecGrp3000",
-        #    ip_protocol='TCP',
-        #    source_security_group_id=self.fargate_load_balanced_service.service.connections.security_groups[0].security_group_id,
-        #    from_port=3000,
-        #    to_port=3000,
-        #    group_id=self.services_3000_sec_group.security_group_id
-        #)
-#
-        ## There has to be a better way, but for now this is what we got.
-        ## Allow inbound 3000 Backend to Frontend Service
-        #self.sec_grp_ingress_frontend_to_backend_3000 = aws_ec2.CfnSecurityGroupIngress(
-        #    self, "InboundFrontendtoBackendSecGrp3000",
-        #    ip_protocol='TCP',
-        #    source_security_group_id=self.services_3000_sec_group.security_group_id,
-        #    from_port=3000,
-        #    to_port=3000,
-        #    group_id=self.fargate_load_balanced_service.service.connections.security_groups[0].security_group_id,
-        #)        
+        self.fargate_load_balanced_service = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "FrontendFargateLBService",
+            cluster=self.base_platform.ecs_cluster,
+            cpu=256,
+            memory_limit_mib=512,
+            desired_count=1,
+            public_load_balancer=True,
+            cloud_map_options=self.base_platform.sd_namespace,
+            task_image_options=self.fargate_task_image
+        )
 
 
 _env = core.Environment(account=getenv('AWS_ACCOUNT_ID'), region=getenv('AWS_DEFAULT_REGION'))
-stack_name = "ecsworkshop-frontend"
+environment = "ecsworkshop"
+stack_name = "{}-frontend".format(environment)
 app = core.App()
 FrontendService(app, stack_name, env=_env)
 app.synth()
